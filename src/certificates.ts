245d6bcea81da3f5bb51240451e74720b94e6dfc
yLorde <davisouza2204@gmail.com>
2026-09-21 03:48:02 -0300

    certificado alura

--- src/certificates.ts
+++ src/certificates.ts
@@ -6,6 +6,11 @@ export const mainCertificates: ICertificates[] = [
         live: ["Ver Certificado", "Ver Emissor"],
         url: ["/certificates/itau/MTBVF70J.pdf", "https://dio.me/certificate/MTBVF70J"]
     },
+    {
+        type: "Imersão", title: "Imersão Engenharia de Dados com IA", code: "5f4ef245-6f6c-4677-9958-5118660f9239",
+        live: ["Ver Certificado", "Ver Emissor"],
+        url: ["/certificates/alura/5f4ef245-6f6c-4677-9958-5118660f9239.pdf", "https://cursos.alura.com.br/immersion/certificate/5f4ef245-6f6c-4677-9958-5118660f9239"]
+    },
 ];
 
 export const certificates: ICertificates[] = [
@@ -31,10 +36,10 @@ export const certificates: ICertificates[] = [
         live: ["Ver Certificado", "Ver Emissor"],
         url: ["certificates/itau/XTAHJBE3.pdf", "https://dio.me/certificate/XTAHJBE3"],
     },
-    {
-        type: "Bootcamp",
-        title: "DIO Agent: Nunca Mais Estude Sozinho", code: "3TOJA4L0",
-        live: ["Ver Certificado", "Ver Emissor"],
-        url: ["certificates/itau/3TOJA4L0.pdf", "https://dio.me/certificate/3TOJA4L0"],
-    },
+    // {
+    //     type: "Bootcamp",
+    //     title: "DIO Agent: Nunca Mais Estude Sozinho", code: "3TOJA4L0",
+    //     live: ["Ver Certificado", "Ver Emissor"],
+    //     url: ["certificates/itau/3TOJA4L0.pdf", "https://dio.me/certificate/3TOJA4L0"],
+    // },
 ];
